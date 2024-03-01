bf25fe5e4192d7ed6c4fcc5024e9a85692f4fa2e
mJesusAha <161620994+mJesusAha@users.noreply.github.com>
2024-03-01 21:06:14 +0600

    Add files via upload

---
+++ src/s21_containers.h
@@ -0,0 +1,8 @@
+#ifndef CPP2_S21_CONTAINERS_SRC_S21_CONTAINERS_H_
+#define CPP2_S21_CONTAINERS_SRC_S21_CONTAINERS_H_
+
+#include "array/s21_array.h"
+#include "list/s21_list.h"
+#include "vector/s21_vector.h"
+
+#endif // #ifndef CPP2_S21_CONTAINERS_SRC_S21_CONTAINERS_H_
